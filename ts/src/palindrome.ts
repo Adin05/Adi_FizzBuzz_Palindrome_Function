92b2fe423593f4296d7718fdd1c1c4cb3bd07fc7
adi nurr <adinurr@gmail.com>
2023-02-10 18:09:28 +0700

    add ;

--- ts/src/palindrome.ts
+++ ts/src/palindrome.ts
@@ -1,11 +1,11 @@
 
 export function Palindrome(input: string){
-    const len = input.length
+    const len = input.length;
 
     for (let i = 0; i < len / 2; i++) {
         if (input[i] !== input[len - 1 - i]) {
-            return 'Bukan Palindrome'
+            return 'Bukan Palindrome';
         }
     }
-    return 'Palindrome'
+    return 'Palindrome';
 }
